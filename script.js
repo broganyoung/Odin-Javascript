e554adc08eb3f0cd1b7da7d23c9c0e04c00beb28
broganyoung <brogan_young@outlook.com>
2022-03-23 20:05:02 +0000

    changed ui of game, still need to work on it, had trouble with idea of changing computer image in a slot machine kind of style, cycling through all images before ending on chosen one

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function gamePlay(){
+let games = 0
+let draws = 0
+let losses = 0
+let wins = 0
+let gameMessage = ""
+
+function gamePlay(inputMove){
 
     let moves = ["rock", "paper", "scissors"]
 
@@ -6,118 +12,102 @@ function gamePlay(){
         let moveN = Math.floor(Math.random() * 3)
         let computerMove = moves[moveN]
         
-        return computerMove
+        return [computerMove, moveN]
     }
 
-    function playerPlay(){
-        let playerMove = prompt("What is your move?")
+    let computerVars = computerPlay()
+    let computerMove = computerVars[0]
+    let moveN = computerVars[1]
 
-        if (playerMove == null ){
-            playerMove = false
+    let playerMove = inputMove
+
+    let computerImagePath = ""
+
+    for (let i = 0; i < 3; i++){
+
+        for (let j = 0; j < 3; j++) {
             
-            return playerMove
-        }
-        else {
-            playerMove = playerMove.toLowerCase()
-
-            while (true){
-                if(moves.indexOf(playerMove) !== -1){
-                    break;
-                }
-                playerMove = prompt("Not a valid move. Try again:")
-                playerMove = playerMove.toLowerCase()
-            }
+            computerImagePath = "images/" + moves[j] + ".png"
         
-            return playerMove
+            document.getElementById("computerImagePath").src = computerImagePath
+            console.log(computerImagePath)
+    
+        }
+
+    }    
+    
+    for (let j = 0; j < (3 - moveN); j++) {
+            
+        computerImagePath = "images/" + moves[j] + ".png"
+    
+        document.getElementById("computerImagePath").src = computerImagePath
+        console.log(computerImagePath)
+
+    }
+    console.log(computerMove)
+
+    let winner = "null"
+
+    if (playerMove == computerMove){
+        winner = "draw"
+    }
+    else if (playerMove == "rock"){
+
+        if (computerMove == "paper"){
+            winner = "computer"
+        }
+
+        if (computerMove == "scissors"){
+            winner = "player"
+        }
+
+    }
+    else if( playerMove == "paper"){
+
+        if (computerMove == "scissors"){
+            winner = "computer"
+        }
+
+        if (computerMove == "rock"){
+            winner = "player"
+        }
+
+    }
+    else if (playerMove == "scissors"){
+
+        if (computerMove == "rock"){
+            winner = "computer"
+        }
+
+        if (computerMove == "paper"){
+            winner = "player"
         }
 
     }
 
-    let games = 0
-    let draws = 0
-    let losses = 0
-    let wins = 0
+    playerMove = playerMove.substring(0, 1).toUpperCase() + playerMove.substring(1)
+    computerMove = computerMove.substring(0, 1).toUpperCase() + computerMove.substring(1)
 
-    let play = true;
-
-    while (true){
-
-        if (games == 0){ 
-            play = confirm("Do you want to play Rock, Paper, Scissors?")
-        }
-        else {
-            play = confirm("Do you want to play again?")
-        }
-
-        if(play == false){
-            break;
-        }
-
-        let computerMove = computerPlay()
-        let playerMove = playerPlay()
-
-        if(playerMove == false){
-            break;
-        }
-    
-        let winner = "null"
-    
-        if (playerMove == computerMove){
-            winner = "draw"
-        }
-        else if (playerMove == "rock"){
-    
-            if (computerMove == "paper"){
-                winner = "computer"
-            }
-    
-            if (computerMove == "scissors"){
-                winner = "player"
-            }
-    
-        }
-        else if( playerMove == "paper"){
-    
-            if (computerMove == "scissors"){
-                winner = "computer"
-            }
-    
-            if (computerMove == "rock"){
-                winner = "player"
-            }
-    
-        }
-        else if (playerMove == "scissors"){
-    
-            if (computerMove == "rock"){
-                winner = "computer"
-            }
-    
-            if (computerMove == "paper"){
-                winner = "player"
-            }
-    
-        }
-    
-        playerMove = playerMove.substring(0, 1).toUpperCase() + playerMove.substring(1)
-        computerMove = computerMove.substring(0, 1).toUpperCase() + computerMove.substring(1)
-    
-        if (winner == "draw"){
-            alert("It's a draw!")
-            draws++
-        }
-        else if (winner == "computer"){
-            alert("You lose! " + computerMove + " beats " + playerMove)
-            losses++
-        }
-        else if (winner == "player"){
-            alert("You win! " + playerMove + " beats " + computerMove)
-            wins++
-        }
-
-        games++
-    
+    if (winner == "draw"){
+        gameMessage = "It's a draw!"
+        draws++
+    }
+    else if (winner == "computer"){
+        gameMessage = "You lose! " + computerMove + " beats " + playerMove
+        losses++
+    }
+    else if (winner == "player"){
+        gameMessage = "You win! " + playerMove + " beats " + computerMove
+        wins++
     }
 
-    alert("Thanks for playing! You played " + games + " games, won " + wins + ", lost " + losses + ", and drew " + draws + ".")
+    games++
+
+    document.getElementById("gameMessage").innerHTML = gameMessage;
+
+    document.getElementById("games").innerHTML = games;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("draws").innerHTML = draws;
+    document.getElementById("losses").innerHTML = losses;
+    
 }
